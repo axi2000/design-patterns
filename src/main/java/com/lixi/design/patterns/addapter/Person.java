cd8595ba8979d23847b5f7ca221746c5a00307ff
李玺 <lixi@quyiyuan.com>
2017-08-16 10:35:26 +0800

    提交适配器模式的Person接口

--- src/main/java/com/lixi/design/patterns/addapter/Person.java
+++ src/main/java/com/lixi/design/patterns/addapter/Person.java
@@ -4,4 +4,7 @@ package com.lixi.design.patterns.addapter;
  * Created by lixi on 17/8/16.
  */
 public interface Person {
+    public void speak();
+    public void run();
+    public void eat();
 }
